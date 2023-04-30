0c4b6cdd3f6089e2212e95eae28e5a293979f4b1
saeed <saeedsaf@gmail.com>
2023-04-30 13:15:04 +0330

    - update exist event

--- kafka-book-library-event-consumer/src/main/java/ir/farbod/consumer/service/BookLibraryEventService.java
+++ kafka-book-library-event-consumer/src/main/java/ir/farbod/consumer/service/BookLibraryEventService.java
@@ -4,10 +4,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import ir.farbod.consumer.entity.BookLibraryEvent;
 import ir.farbod.consumer.repository.BookLibraryEventRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @Slf4j
@@ -20,15 +23,33 @@ public class BookLibraryEventService {
     public void processEvent(ConsumerRecord<Integer, String> consumerRecord) throws JsonProcessingException {
         BookLibraryEvent bookLibraryEvent = objectMapper.readValue(consumerRecord.value(), BookLibraryEvent.class);
 
-        switch (bookLibraryEvent.getLibraryEventType()){
+        switch (bookLibraryEvent.getLibraryEventType()) {
             case NEW -> {
                 save(bookLibraryEvent);
                 break;
             }
-            case UPDATE -> {}
+            case UPDATE -> {
+                validate(bookLibraryEvent);
+                save(bookLibraryEvent);
+            }
             default -> log.info("Invalid LibraryEventType ==> {}", bookLibraryEvent.getLibraryEventType());
         }
 
+    }
+
+    private void validate(BookLibraryEvent bookLibraryEvent) {
+        if (bookLibraryEvent.getEventId() == null)
+            throw new IllegalArgumentException("eventId is missing");
+
+        BookLibraryEvent entity = null;
+        try{
+            entity = getByEventId(bookLibraryEvent.getEventId());
+        }
+        catch (Exception e){
+            throw new IllegalArgumentException("Not a valid eventId");
+        }
+
+        log.info("Validation is successfull for eventId : {} ", entity.getEventId());
     }
 
     private void save(BookLibraryEvent bookLibraryEvent) {
@@ -36,6 +57,14 @@ public class BookLibraryEventService {
         bookLibraryEventRepository.save(bookLibraryEvent);
 
         log.info("Successfully persisted the Book Library Event {} ", bookLibraryEvent);
+    }
+
+    public BookLibraryEvent getByEventId(Integer eventId) {
+        Optional<BookLibraryEvent> res = bookLibraryEventRepository.findById(eventId);
+        if (!res.isPresent())
+            throw new EntityNotFoundException("BookEventLibrary not found with eventId " + eventId);
+
+        return res.get();
     }
 
 }
